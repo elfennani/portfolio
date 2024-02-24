5f65e1a1dae86031fa95358130d61536045dcf23
Nizar Elfennani <elfennani2002@gmail.com>
2024-02-24 21:01:22 +0100

    Fully functional contact form

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,93 @@
-import React from "react";
+"use client";
+import React, { FormEventHandler, useState } from "react";
 import Title from "./Title";
 import Input from "./Input";
 import Link from "./Link";
 
-type Props = {};
+const Footer = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-const Footer = (props: Props) => {
+  const [isSending, setIsSending] = useState(false);
+  const [response, setResponse] = useState<string | null>(null);
+
+  type SubmitEvent = FormEventHandler<HTMLFormElement>;
+  const onSubmitHandler: SubmitEvent = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setResponse(null);
+
+    if (!email.trim() || !message.trim()) {
+      setResponse("Email and Details are required!");
+      return;
+    }
+    setIsSending(true);
+
+    try {
+      await fetch("https://formcarry.com/s/j9XyeldV8zn", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, message }),
+      });
+
+      setResponse("I received your submission, thank you!");
+    } catch (error) {
+      setResponse("Failed to send your submission, try again!");
+    } finally {
+      setIsSending(false);
+    }
+  };
+
   return (
     <footer className="flex flex-col gap-2">
       <form
         id="contact"
         className="bg-zinc-100 p-6 flex flex-col gap-6 rounded"
+        onSubmit={onSubmitHandler}
+        action="https://formcarry.com/s/j9XyeldV8zn"
+        method="POST"
+        encType="multipart/form-data"
       >
         <div className="flex flex-col gap-3">
           <Title>Looking for help?</Title>
           <p className="text-zinc-500">Send me details now</p>
         </div>
-        <Input label="Name" placeholder="John Doe" name="name" />
-        <Input label="Email" placeholder="example@abcd.xyz" name="email" />
         <Input
-          label="Details"
+          label="Name"
+          value={name}
+          onChangeText={setName}
+          placeholder="John Doe"
+          name="name"
+        />
+        <Input
+          label="Email*"
+          value={email}
+          onChangeText={setEmail}
+          placeholder="example@abcd.xyz"
+          name="email"
+        />
+        <Input
+          label="Details*"
+          value={message}
+          onChangeText={setMessage}
           placeholder="Enter your message"
           name="details"
           textarea
         />
-        <button
-          type="submit"
-          className="text-zinc-700 self-end font-medium leading-6 underline transition-colors hover:text-zinc-500"
-        >
-          Submit
-        </button>
+        <div className="flex justify-between items-center">
+          <p className="font-medium">{response}</p>
+          <button
+            type="submit"
+            className="text-zinc-700 disabled:opacity-50 font-medium leading-6 underline transition-colors hover:text-zinc-500"
+            disabled={isSending}
+          >
+            Submit
+          </button>
+        </div>
       </form>
       <nav className="flex justify-between py-6 max-[932px]:flex-col max-[932px]:items-center gap-6">
         <p className="text-zinc-500">Made with Next.js</p>
--- components/Input.tsx
+++ components/Input.tsx
@@ -3,6 +3,8 @@ interface InputProps {
   placeholder: string;
   name: string;
   textarea?: boolean;
+  onChangeText?: (text: string) => void;
+  value?: string;
 }
 const Input = (props: InputProps) => {
   const Component = props.textarea ? "textarea" : "input";
@@ -11,6 +13,8 @@ const Input = (props: InputProps) => {
       {props.label}
       <Component
         type="text"
+        value={props.value}
+        onChange={(e) => props.onChangeText?.(e.target.value)}
         name={props.name}
         placeholder={props.placeholder}
         className="w-full px-4 py-3 max-sm:p-2 rounded font-normal bg-white border border-zinc-200 text-base placeholder:text-zinc-300 outline-none focus:outline focus:outline-black focus:border-black outline-offset-0 outline-1"
